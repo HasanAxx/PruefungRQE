33a7d52b146e8dc8922af0c3ef9122240d632932
Hasan Aras <wi19b086@technikum-wien.at>
2020-11-05 10:12:36 +0100

    Projektdateien hinzufügen.

---
+++ PruefungRQE/SpecFlowFeature1.feature
@@ -0,0 +1,21 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Sinus
+    Given the first number is PI
+    When sinus is taken from this number
+    Then the result should be 0
+
+Scenario: Cosinus
+    Given the first number is PI
+    When cosinus is taken from this number
+    Then the result should be -1
+
+Scenario: tangens
+    Given the first number is PI
+    When tangens is taken from this number
+    Then the result should be 0
+
---
+++ PruefungRQE/SpecFlowFeature1Steps.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace PruefungRQE
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        double number, taken;
+        [Given(@"the first number is PI")]
+        public void GivenTheFirstNumberIsPI()
+        {
+            number = Math.PI;
+        }
+        
+        [When(@"sinus is taken from this number")]
+        public void WhenSinusIsTakenFromThisNumber()
+        {
+            taken = Math.Sin(number);
+        }
+        
+        [When(@"cosinus is taken from this number")]
+        public void WhenCosinusIsTakenFromThisNumber()
+        {
+            taken = Math.Cos(number);
+        }
+        
+        [When(@"tangens is taken from this number")]
+        public void WhenTangensIsTakenFromThisNumber()
+        {
+            taken = Math.Tan(number);
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(double p0)
+        {
+            Assert.Equal(p0, taken, 5);
+            
+        }
+    }
+}
